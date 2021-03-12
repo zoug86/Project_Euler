f4e32518023c56324b434d0bc1e1923b6ec315b1
Houssem Marzougui <marzouguih86@gmail.com>
2021-03-11 21:13:19 -0800

    Euler-1-Solved

--- main.py
+++ main.py
@@ -4,3 +4,14 @@
 # multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
+sum1 = 0
+sum2 = 0
+for num in range(3,1000):
+    if num % 3 == 0:
+        sum1 += num
+    if num % 5 == 0 and num % 3 != 0:
+        sum2 += num
+
+total = sum1 + sum2
+
+print(total)
