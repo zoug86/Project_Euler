c26e5f9ceac698ea9bfa1f17f586e7be1a67496b
Houssem Marzougui <marzouguih86@gmail.com>
2021-03-14 19:11:02 -0700

    Euler-6-Solved

--- main.py
+++ main.py
@@ -93,6 +93,8 @@ print(max(palindrome_arr))
 # from 1 to 10 without any remainder. What is the smallest positive number that is
 # evenly divisible by all of the numbers from 1 to 20?
 
+
+'''
 num = 2520 * 11 * 13 * 17 * 19
 
 count = 0
@@ -105,3 +107,38 @@ while num > 1:
     num += 1
     count = 0
 print(num)
+'''
+
+# Date: March 14 2021
+# Euler 6: Sum square difference
+# Problem: The sum of the squares of the first ten natural numbers is:
+# 1^2 + 2^2 + ... + 10^2 = 355
+#
+# The square of the sum of the first ten natural numbers is:
+# (1 + 2 + ... + 10)^2 = 55^2 = 3025
+# Hence the difference between the sum of the squares of the first ten natural
+# numbers and the square of the sum is: 3025 - 385 = 2640
+#
+# Find the difference between the sum of the squares of the first one hundred
+# natural numbers and the square of the sum.
+
+
+# we should use n*(n+1)/2
+
+sum1 = 0
+for i in range(1, 101):
+    sum1 += i*i
+
+sum2 = (100*101//2)*(100*101//2)
+
+diff = sum2 - sum1
+
+print(diff)
+
+
+
+
+
+
+
+
