cce69a5110ea237585e0aebb3bba043e8f911704
Houssem Marzougui <marzouguih86@gmail.com>
2021-03-16 23:06:30 -0700

    Euler-7-solved

--- main.py
+++ main.py
@@ -125,6 +125,7 @@ print(num)
 
 # we should use n*(n+1)/2
 
+'''
 sum1 = 0
 for i in range(1, 101):
     sum1 += i*i
@@ -134,11 +135,25 @@ sum2 = (100*101//2)*(100*101//2)
 diff = sum2 - sum1
 
 print(diff)
+'''
 
+# Date: March 15 2021
+# Euler 7: 10001st prime
+# Problem: By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13,
+# we can see that the 6th prime is 13. What is the 10 001st prime number?
 
+arr = [1,2]
+i = 3
+count = 0
+while len(arr) < 10002:
+    for j in arr:
+        if i%j != 0:
+            count += 1
+    if count == len(arr) - 1:
+        arr.append(i)
+    count = 0
+    i += 1
 
-
-
-
+print(arr[10001])
 
 
